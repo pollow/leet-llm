c2b7e8b472d3478ac71154d1724955861be85631
Chang Xing <pollow.xc@gmail.com>
2026-05-31 13:33:03 -0700

    solution for 007

--- 007_topk/solution.py
+++ 007_topk/solution.py
@@ -6,8 +6,14 @@ import numpy as np
 
 
 def argmax(x: np.ndarray, axis: int = -1) -> np.ndarray:
-    raise NotImplementedError("reference solution not written yet")
+    return np.argmax(x, axis=axis)
 
 
 def top_k(x: np.ndarray, k: int) -> tuple[np.ndarray, np.ndarray]:
-    raise NotImplementedError("reference solution not written yet")
+    idx = np.argpartition(x, -k, axis=-1)[..., -k:]
+    vals = np.take_along_axis(x, idx, axis=-1)
+
+    order = np.argsort(vals, axis=-1)[..., ::-1]
+    vals = np.take_along_axis(vals, order, axis=-1)
+    idx = np.take_along_axis(idx, order, axis=-1)
+    return (vals, idx)
